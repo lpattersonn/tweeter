aa19f73159935bc969c6a6779917b4f1441b4cf8
Leshan Patterson <leshan0627@gmail.com>
2021-12-15 08:00:55 +0000

    Updates to tweet container and completed character count

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  // "keypress registers a keyboard character except for arrow entries"
+  const $tweetText = $("#tweet-text");
+  let tweetValue = $(".counter")[0];
+  tweetValue.value = 140;
+
+  $tweetText.on("keypress", function tweetReduceChar(e) {
+    if (this) {
+      tweetValue.value--;
+    }
+    if (tweetValue.value >= 0) {
+      $(".counter").css({ color: "black" });
+    }
+    if (tweetValue.value < 0) {
+      $(".counter").css({ color: "red" });
+    }
+  });
+  $tweetText.on("keydown", function tweetIncreaseChar(e) {
+    const key = e.key;
+    if (key === "Backspace" && tweetValue.value < 140) {
+      tweetValue.value++;
+    }
+  });
+});
+/* 
+  const $button = $("#update-button");
+
+  $button.on("click", function () {
+    console.log(this);
+  }); */
+
+//const $newChannelInput = $('#new-channel-input');
